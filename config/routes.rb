4d822688a7c09883f8c7f1b4d4f208d04091c710
Sarah Sehr <sarah.sehr@appfolio.com>
2023-03-17 14:48:14 -0700

    Move Accounting routes to inside accounting pack
    Not sure if this causes violations because it breaks bin/packwerk
    check
    
    Error:
    Your router tried to #draw the external file accounting.rb, (ArgumentError)
    but the file was not found in:
    
     * /Users/sarah.sehr/src/packwerk_test_app/config/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'work_orders/index'
-  get 'bills/index'
   get 'articles/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  draw(:accounting)
 end
